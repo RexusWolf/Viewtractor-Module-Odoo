e3ffdd5b38d769d19b6764d8862941039374d0e4
rexuswolf <francisjmp99@gmail.com>
2019-05-11 21:28:36 +0200

    Finished v1.0

---
+++ __manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "viewtractor",
+
+    'summary': """
+       Herencia de template del Home del Sitio Web""",
+
+    'description': """
+       Con este módulo, importamos la página inicial (home) de una página mediante
+       una herencia de template (views/homeview.xml).
+    """,
+
+    'author': "rexuswolf",
+    'website': "github.com/rexuswolf",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Importación',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/homeview.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
